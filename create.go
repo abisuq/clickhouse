ef3635b11be68ce1ba69281dcba027e895c69cf4
__ <chenrunshan@outlook.com>
2021-11-16 14:12:54 +0800

    Update create.go

--- create.go
+++ create.go
@@ -17,31 +17,28 @@ func Create(db *gorm.DB) {
 		if db.Statement.SQL.String() == "" {
 			db.Statement.SQL.Grow(180)
 			db.Statement.AddClauseIfNotExists(clause.Insert{})
+			values := callbacks.ConvertToCreateValues(db.Statement)
+			
+			prepareValues := clause.Values{
+				Columns: values.Columns,
+				Values:  [][]interface{}{values.Values[0]},
+			}
+			db.Statement.AddClause(prepareValues)
+			db.Statement.Build("INSERT", "VALUES", "ON CONFLICT")
 
-			if values := callbacks.ConvertToCreateValues(db.Statement); len(values.Values) > 1 {
-				prepareValues := clause.Values{
-					Columns: values.Columns,
-					Values:  [][]interface{}{values.Values[0]},
-				}
-				db.Statement.AddClause(prepareValues)
-				db.Statement.Build("INSERT", "VALUES", "ON CONFLICT")
+			stmt, err := db.Statement.ConnPool.PrepareContext(db.Statement.Context, db.Statement.SQL.String())
+			if db.AddError(err) != nil {
+				return
+			}
+			defer stmt.Close()
 
-				stmt, err := db.Statement.ConnPool.PrepareContext(db.Statement.Context, db.Statement.SQL.String())
-				if db.AddError(err) != nil {
+			for _, value := range values.Values {
+				if _, err := stmt.Exec(value...); db.AddError(err) != nil {
 					return
 				}
-				defer stmt.Close()
-
-				for _, value := range values.Values {
-					if _, err := stmt.Exec(value...); db.AddError(err) != nil {
-						return
-					}
-				}
-				return
-			} else {
-				db.Statement.AddClause(values)
-				db.Statement.Build("INSERT", "VALUES", "ON CONFLICT")
 			}
+			return
+			
 		}
 
 		if !db.DryRun && db.Error == nil {
